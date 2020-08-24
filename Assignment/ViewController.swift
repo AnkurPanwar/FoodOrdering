9229f736c3d54367e46201bc6b9d8be97e052361
Ankur <panwarankur23@gmail.com>
2020-08-24 22:45:56 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Ankur on 24/08/20.
+//  Copyright © 2020 Ankur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
